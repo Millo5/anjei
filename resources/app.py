b5cae584d2662cf54d138fb5d29270878877ecee
Millo <23leeuw@gmail.com>
2026-06-16 15:09:41 +0200

    packager

--- resources/app.py
+++ resources/app.py
@@ -3,30 +3,30 @@ from df.main import Template, Block, Item
 from pathlib import Path
 
 SCRIPT_DIR = Path(__file__).parent
-ELEMENTS_DIR = SCRIPT_DIR / "elements.json"
-FORMS_DIR = SCRIPT_DIR / "forms.json"
+ITEMS_DIR = SCRIPT_DIR / "items.json"
+RECIPES_DIR = SCRIPT_DIR / "recipes.json"
 
 
 
 def main():
-    elements = json.load(ELEMENTS_DIR.open())
-    forms = json.load(FORMS_DIR.open())
+    items = json.load(ITEMS_DIR.open())
+    recipes = json.load(RECIPES_DIR.open())
 
     t = Template("const.data")
-    t.add_parameter("data", "var")
-    t.add_parameter("forms", "var")
+    t.add_parameter("items", "var")
+    t.add_parameter("recipes", "var")
 
-    items = []
-    for element in elements:
-        items.append(Item.String(json.dumps(element), True))
+    args = []
+    for item in items:
+        args.append(Item.String(json.dumps(item), True))
 
-    t.add_block(Block.create_list("data", items))
+    t.add_block(Block.create_list("items", args))
 
-    items = []
-    for form in forms:
-        items.append(Item.String(json.dumps(form), True))
+    args = []
+    for recipe in recipes:
+        args.append(Item.String(json.dumps(recipe), True))
     
-    t.add_block(Block.create_list("forms", items))
+    t.add_block(Block.create_list("recipes", args))
 
     t.to_millomod()
 
